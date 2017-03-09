89b98ac2ecb92c211d181454ac2145291da5b76b
cprater <cprater23@gmail.com>
2017-03-09 09:05:41 -0700

    Debugging IBInspectable

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -10,13 +10,22 @@ import UIKit
 
 @IBDesignable class RatingControl: UIStackView {
     //MARK: Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+    @IBInspectable var starCount: Int = 5
+    
     private var ratingButtons = [UIButton]()
+    
     var rating = 0
     
     
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
+    }
+    
+    override init(arrangedSubviews: [UIView]) {
+        super.init(arrangedSubviews: arrangedSubviews)
         setupButtons()
     }
     
@@ -32,15 +41,15 @@ import UIKit
     
     //MARK: Private Methods
     private func setupButtons() {
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             // Create button
             let button = UIButton()
             button.backgroundColor = UIColor.redColor()
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraintEqualToConstant(44.0).active = true
-            button.widthAnchor.constraintEqualToConstant(44).active = true
+            button.heightAnchor.constraintEqualToConstant(starSize.height).active = true
+            button.widthAnchor.constraintEqualToConstant(starSize.width).active = true
             
             // Setup button action
             button.addTarget(self,
