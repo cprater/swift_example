e755674424e1ac8cff12ca0c98f90d3405f62c1e
cprater <cprater23@gmail.com>
2017-03-14 17:55:18 -0600

    Created model and unit tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,33 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Christopher Prater on 3/14/17.
+//
+//
+
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Fail if props are empty
+        
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
